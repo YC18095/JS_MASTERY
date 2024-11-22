ede7c4fb1639e9ad58cf63619fc9de9848841fa2
Dung Tran Chi <dung.tran@titancorp.vn>
2024-11-22 16:51:20 +0700

    m

--- Storeit/storeit/app/(auth)/sign-up/page.tsx
+++ Storeit/storeit/app/(auth)/sign-up/page.tsx
@@ -1,7 +1,8 @@
+import AuthForm from "@/components/AuthForm";
 
 
 const SignUp = () => {
-    return   <div>Sign Up</div>
+    return   <AuthForm type="sign-up"/>
     
 }
 
---
+++ Storeit/storeit/components/AuthForm.tsx
@@ -0,0 +1,8 @@
+
+type FormType = "sign-in" | "sign-up"
+
+const AuthForm = ({ type }: { type: FormType}) => {
+    return <div>AuthForm</div>
+}
+
+export default AuthForm
